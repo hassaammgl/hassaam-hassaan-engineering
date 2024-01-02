a6b70e44c1d552052945ea4a3e72ccbeb09277a1
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-02 23:48:06 +0500

    dasfasfadf

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import company from "@/public/companylogo.png";
+import Image from 'next/image';
+import style from "@/styles/header.module.scss";
 
 const Header = () => {
   return (
-    <div>
-      Blue Color
+    <div className={style.header}>
+        <Image src={company} />
     </div>
   )
 }
