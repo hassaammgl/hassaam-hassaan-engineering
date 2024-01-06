d13aa64d654eb05213a2920772841e94563ba5e8
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-06 19:46:14 +0500

    dasfasfdasf

--- components/Quotation.jsx
+++ components/Quotation.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { FaCirclePlay } from "react-icons/fa6";
 import style from "@/styles/quotation.module.scss";
 import Button from "./Button";
+import { font } from "./fonts";
 
 const Quotation = () => {
   return (
     <div className={style.quotation}>
-      <div className={style.left}>
+      <div className={`${style.left} ${font.ubuntu.className}`}>
         <h2>Gets a Quotation For <br /> Your Work</h2>
       </div>
       <div className={style.right}>
