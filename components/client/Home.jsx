8121fe501a1c557c473f539159db7013026e4609
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-08 01:08:26 +0500

    solo

--- components/client/Home.jsx
+++ components/client/Home.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useBox, useSphere, Physics, usePlane } from "@react-three/cannon";
 import React, { useRef, Suspense } from "react";
 import ServerHome from "../server/Home";
 import { PiPhoneCall } from "react-icons/pi";
@@ -33,7 +34,10 @@ const Home = () => {
 
 const Right = () => {
   return (
-    <Canvas className={style.right}>
+    <Canvas
+      camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 4, 7] }}
+      className={style.right}
+    >
       <OrbitControls />
       <ambientLight />
       <Scene />
@@ -48,29 +52,88 @@ const Right = () => {
 //   }
 // });
 
-const Modal = () => {
+const Modals = () => {
   return (
-    <group>
-      <mesh>
-        <sphereGeometry />
-        <meshStandardMaterial attach="material" wireframe color="red" />
-      </mesh>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial attach="material" wireframe color="green" />
-      </mesh>
-      <mesh>
-        <torusGeometry />
-        <meshStandardMaterial attach="material" wireframe color="gray" />
-      </mesh>
-    </group>
+    <Physics>
+      <Torus />
+      <Cube />
+      <Sphere />
+      <Dodecahedron />
+      <Octahedron />
+      <Lathe />
+      <Plane />
+    </Physics>
+  );
+};
+
+const Torus = ({ x, y, z }) => {
+  const [ref] = useSphere(() => ({ mass: 12, position: [0, 10, 0] }));
+  return (
+    <mesh ref={ref} position={[4, 10, 0]}>
+      <torusGeometry />
+      <meshStandardMaterial attach="material" color="gray" />
+    </mesh>
+  );
+};
+const Dodecahedron = ({ x, y, z }) => {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 10, 0] }));
+  return (
+    <mesh ref={ref} position={[6, 10, 0]}>
+      <dodecahedronGeometry />
+      <meshStandardMaterial attach="material" color="blue" />
+    </mesh>
+  );
+};
+const Octahedron = ({ x, y, z }) => {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 10, 0] }));
+  return (
+    <mesh ref={ref} position={[8, 10, 0]}>
+      <octahedronGeometry />
+      <meshStandardMaterial attach="material" color="yellow" />
+    </mesh>
+  );
+};
+const Lathe = ({ x, y, z }) => {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 10, 0] }));
+  return (
+    <mesh ref={ref} position={[8, 10, 0]}>
+      <latheGeometry />
+      <meshStandardMaterial attach="material" color="black" />
+    </mesh>
+  );
+};
+const Sphere = ({ x, y, z }) => {
+  const [ref] = useSphere(() => ({ mass: 12, position: [0, 10, 0] }));
+  return (
+    <mesh ref={ref} position={[0, 10, 0]}>
+      <sphereGeometry />
+      <meshStandardMaterial roughness={0.2} attach="material" color="red" />
+    </mesh>
+  );
+};
+const Cube = ({ x, y, z }) => {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 10, 0] }));
+  return (
+    <mesh ref={ref} position={[2, 10, 0]}>
+      <boxGeometry />
+      <meshStandardMaterial attach="material" color="green" />
+    </mesh>
+  );
+};
+const Plane = ({ x, y, z }) => {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
+  return (
+    <mesh ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, -5]}>
+      <planeGeometry args={[100, 100]} />
+      <meshStandardMaterial attach="material" color="#403e51" />
+    </mesh>
   );
 };
 
 const Scene = () => {
   return (
     <Suspense fallback={null}>
-      <Modal />
+      <Modals />
     </Suspense>
   );
 };
