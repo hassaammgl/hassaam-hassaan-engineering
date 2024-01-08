c1852c8d0c24f618cb63e964d8938dac38d22202
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-09 00:01:04 +0500

    I am start liking my own skills

--- components/client/Services.jsx
+++ components/client/Services.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import Image from 'next/image';
+import client from "@/Images/girl.jpg";
 import style from "@/styles/services.module.scss";
 
 const Services = () => {
   return (
     <div className={style.services}>
         <div className={style.left}>
-            SERVICES
+          leftsection
         </div>
         <div className={style.right}>
           <div className={style.rightcontent}>
-            adsfkjasdlkfjdsf
+          <Image className={style.clientimg} src={client} alt='client' />
           </div>
         </div>
     </div>
