d9eb00d60371236f0699deeba263e1db007375f9
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-09 00:19:09 +0500

    as I am trying to lose weight

--- components/client/Services.jsx
+++ components/client/Services.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import client from "@/Images/girl.jpg";
 import style from "@/styles/services.module.scss";
 
 const Services = () => {
   return (
     <div className={style.services}>
-        <div className={style.left}>
-          leftsection
-        </div>
-        <div className={style.right}>
-          <div className={style.rightcontent}>
-          <Image className={style.clientimg} src={client} alt='client' />
+      <div className={style.left}>leftsection</div>
+      <div className={style.right}>
+        <div className={style.rightcontent}>
+          <Image className={style.clientimg} src={client} alt="client" />
           <h3>BUYER</h3>
           <h2>JONI HENA</h2>
-          <p> I feel very happy and proud to connect with this company. I prefer to anyone they done their work from this platform. I wish very good luck & success for this Company. </p>
-          </div>
+          <p>
+            {" "}
+            I feel very happy and proud to connect with this company. I prefer
+            to anyone they done their work from this platform. I wish very good
+            luck & success for this Company.{" "}
+          </p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
