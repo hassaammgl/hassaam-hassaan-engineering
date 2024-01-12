f9e7bb0829a36eb80540c7104f4af5c4202be8c6
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-12 22:08:53 +0500

    dsfadsf

--- components/client/Services.jsx
+++ components/client/Services.jsx
@@ -5,10 +5,40 @@ import style from "@/styles/services.module.scss";
 const Services = () => {
   return (
     <div className={style.service}>
-      <div className={style.servicecontent}>
         <h1>
           We Provide Best Services <br /> For Industries
         </h1>
+      <div className={style.servicecontent}>
+        <div className={style.container}>
+          <div className={style.box}>
+            <div className={style.icon}>
+              <MdAddIcCall />
+            </div>
+            <div className={style.boxcontainer}>
+              <h2>Power & Energy</h2>
+              <p>
+                Lorem ipsum dolor sit amet & consectetur adipisicing elit.
+                Excepturi, voluptas cum.
+              </p>
+            </div>
+            <h2></h2>
+          </div>
+        </div>
+        <div className={style.container}>
+          <div className={style.box}>
+            <div className={style.icon}>
+              <MdAddIcCall />
+            </div>
+            <div className={style.boxcontainer}>
+              <h2>Power & Energy</h2>
+              <p>
+                Lorem ipsum dolor sit amet & consectetur adipisicing elit.
+                Excepturi, voluptas cum.
+              </p>
+            </div>
+            <h2></h2>
+          </div>
+        </div>
         <div className={style.container}>
           <div className={style.box}>
             <div className={style.icon}>
