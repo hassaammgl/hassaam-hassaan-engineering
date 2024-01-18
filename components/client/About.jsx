9adaac3fc47ff8dc065d5abc2100600c6f15254d
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-18 19:33:36 +0500

    dafdasf

--- components/client/About.jsx
+++ components/client/About.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import style from "@/styles/about.module.scss";
+import Image from 'next/image';
+import img from "@/Images/girl.jpg";
 
 const About = () => {
   return (
     <div className={style.about}>
-        lkjhgfdsa
+        <div className={style.left}>
+          <Image src={img} alt='aboutimg' />
+        </div>
+        <div className={style.right}></div>
     </div>
   )
 }
 
-export default About
+export default About;
