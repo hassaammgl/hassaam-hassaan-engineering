9e654fd4b164787a354a7d8a559d24511ffa7e12
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-18 20:15:13 +0500

    dafdasf

--- components/client/About.jsx
+++ components/client/About.jsx
@@ -15,7 +15,11 @@ const About = () => {
           <h2>Providing Full Range of High Services Solution Worldwide</h2>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, impedit qui? Ut, ratione. Esse, fuga. Cupiditate excepturi possimus alias.</p>
           <div className={style.container}>
-            <div className={style.box}></div>
+            <div className={style.box}>
+              <div className={style.icon}></div>
+              <h3>Worldwide Manufacturing</h3>
+              <p className={style.p} >Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </div>
           </div>
         </div>
       </div>
