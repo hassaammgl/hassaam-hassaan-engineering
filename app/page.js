c73be9bad989e3c3efc5c805de49b5c3254cde17
hassaammgl <h455a4mmehtab@gmail.com>
2024-02-01 01:42:33 +0500

    adf

--- app/page.js
+++ app/page.js
@@ -14,12 +14,12 @@ export default function Home() {
       {/* <Header /> */}
       {/* <Main /> */}
       {/* <Client /> */}
-      {/* <Quotation /> */}
-      <Achievement />
+      <Quotation />
+      {/* <Achievement />
       <Services />
       <Gallery />
       <About />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
