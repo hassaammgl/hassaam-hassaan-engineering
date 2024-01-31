0858deccb283d88566e74af8ee2be4e3615784ae
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-31 23:34:07 +0500

    sadf

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
       {/* <Achievement /> */}
       {/* <Services /> */}
       {/* <Gallery /> */}
-      {/* <About /> */}
+      <About />
       <Footer />
     </>
   );
