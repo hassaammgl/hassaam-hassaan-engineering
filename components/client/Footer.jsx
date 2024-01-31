ddc0617f4e45a09ccb578ceb89688bff32b85f58
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-31 17:44:43 +0500

    this is your time

--- components/client/Footer.jsx
+++ components/client/Footer.jsx
@@ -3,7 +3,16 @@ import style from "@/styles/footer.module.scss";
 
 const Footer = () => {
   return (
-    <div className={style.footer}>This is a Footer Section</div>
+    <div className={style.footer}>
+      <div className={style.left}>
+        <div className={style.left1}></div>
+        <div className={style.left2}></div>
+      </div>
+      <div className={style.right}>
+        <div className={style.right1}></div>
+        <div className={style.right2}></div>
+      </div>
+    </div>
   )
 }
 
