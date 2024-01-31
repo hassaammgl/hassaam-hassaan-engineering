886e17529b14fe1ee97570cfce702f2cd08768f0
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-31 17:49:43 +0500

    adsfadsf

--- components/client/Footer.jsx
+++ components/client/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import style from "@/styles/footer.module.scss";
+import Image from 'next/image';
+import logo from "@/public/companylogo.png";
 
 const Footer = () => {
   return (
     <div className={style.footer}>
       <div className={style.left}>
-        <div className={style.left1}></div>
+        <div className={style.left1}>
+          <Image src={logo}  alt='' />
+        </div>
         <div className={style.left2}></div>
       </div>
       <div className={style.right}>
