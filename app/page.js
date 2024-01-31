43d28ea5796d79577c4c3449ddd0ab5015726c31
hassaammgl <h455a4mmehtab@gmail.com>
2024-02-01 00:29:38 +0500

    fgfdg

--- app/page.js
+++ app/page.js
@@ -17,9 +17,9 @@ export default function Home() {
       {/* <Quotation /> */}
       {/* <Achievement /> */}
       {/* <Services /> */}
-      {/* <Gallery /> */}
+      <Gallery />
       <About />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
