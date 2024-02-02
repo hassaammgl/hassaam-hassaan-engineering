d19f746bc3205f05d873518bcbdc9d9c06dbdf32
hassaammgl <h455a4mmehtab@gmail.com>
2024-02-02 16:33:05 +0500

    dasf

--- components/client/ProductDetails.jsx
+++ components/client/ProductDetails.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import "@/styles/productdetails.module.scss";
+import style from  "@/styles/productdetails.module.scss";
+import Image from 'next/image';
+import bg from "@/Images/girl.jpg";
 
 const Product = () => {
   return (
     <>
     <div className={style.productdetail}>
-      
+      <div className={style.box}>
+        <Image src={bg} alt='girlimg'  />
+        <div className={style.content}>
+          <p>This Product is made for the purpose for the living things</p>
+        </div>
+      </div>
     </div>
     </>
   )
