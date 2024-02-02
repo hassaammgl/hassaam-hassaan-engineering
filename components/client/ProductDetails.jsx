4297342c06bde8d0b1b16d663a55e2d751fc1033
hassaammgl <h455a4mmehtab@gmail.com>
2024-02-02 21:43:05 +0500

    asdf

--- components/client/ProductDetails.jsx
+++ components/client/ProductDetails.jsx
@@ -6,12 +6,15 @@ import bg from "@/Images/girl.jpg";
 const Product = () => {
   return (
     <>
-    <div className={style.productdetail}>
-      <div className={style.box}>
-        <Image src={bg} alt='girlimg'  />
-        <div className={style.content}>
-          <p>This Product is made for the purpose for the living things</p>
+    <div className={style.card}>
+      <Image src={bg} class={style.cardimg} alt="" />
+      <div className={style.cardbody}>
+        <h1 className={style.cardtitle}>HHE</h1>
+        <p className={style.cardsubtitle}>Your Dream Come True Company</p>
+        <div className={style.cardinfo}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, molestias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, hic?
         </div>
+        <button className={style.cardbtn}>View Full Plan</button>
       </div>
     </div>
     </>
