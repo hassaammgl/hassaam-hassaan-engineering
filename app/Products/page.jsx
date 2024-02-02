d4c3bff3e2f7d80933ed95488692389c5b4e08a2
hassaammgl <h455a4mmehtab@gmail.com>
2024-02-02 16:24:10 +0500

    asdf

--- app/Products/page.jsx
+++ app/Products/page.jsx
@@ -4,6 +4,7 @@ import style from "@/styles/product.module.scss";
 import Footer from "@/components/Client/Footer";
 import Header from "@/components/Header";
 import React from "react";
+import ProductDetails from "@/components/Client/ProductDetails";
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Header />
         <div className={style.product}>
             <h2>Our Products</h2>
+            <ProductDetails />
         </div>
         <Footer />
         </>
---
+++ components/client/ProductDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "@/styles/productdetails.module.scss";
+
+const Product = () => {
+  return (
+    <>
+    <div className={style.productdetail}>
+      
+    </div>
+    </>
+  )
+}
+
+export default Product
