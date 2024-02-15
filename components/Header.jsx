b254feed861d3d14619d4cce79d8187b9413b8f1
hassaammgl <h455a4mmehtab@gmail.com>
2024-02-15 21:07:31 +0500

    asdf

--- components/Header.jsx
+++ components/Header.jsx
@@ -59,8 +59,7 @@ const Header = () => {
       <div className={style.links}>
         <div className={`${style.link} ${roboto.className}`}>
           <Link href={"/"}>Home</Link>
-          {/* <Link href={"/Products"}>Products</Link> */}
-          <Link href={"/Contact"}>Contact Us</Link>
+          <Link href={"/Products"}>Products</Link>
         </div>
         <Button name={"Get a Quote"} />
       </div>
@@ -86,8 +85,7 @@ const Header = () => {
           <div className={style.link}>
             <Link href={"/"}>Home</Link>
             <Link href={"/Services"}>Services</Link>
-            {/* <Link href={"/Products"}>Products</Link> */}
-            <Link href={"/Contact"}>Contact Us</Link>
+            <Link href={"/Products"}>Products</Link>
           </div>
         )}
       </div>
